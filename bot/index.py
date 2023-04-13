00f96aab8011196919a551de65b1df162d42eda9
alexis.poinsignon@ynov.com <alexis@LAPTOP-VOLBVSA3.localdomain>
2023-04-13 17:39:48 +0200

    Ajout DB SQLITE, Stockage iCal, iCal Time, Récuperation Ical, Rappel, ChatGPT

--- bot/index.py
+++ bot/index.py
@@ -1,14 +1,92 @@
 import discord
 from discord.ext import commands
 import asyncio
+import openai
+import requests
+import icalendar
+import pytz
+from datetime import datetime, timedelta
+import sqlite3
+import re
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
+openai.api_key = ""
 
-pedagogie_emoji = "🎓" # définir un emoji pour le rôle Pédagogie
-sos_emoji = "🆘" # définir un emoji pour le rôle SOS
+pedagogie_emoji = "🎓"  # définir un emoji pour le rôle Pédagogie
+sos_emoji = "🆘"  # définir un emoji pour le rôle SOS
+
+# url = "https://hp22.ynov.com/LYO/Telechargements/ical/Edt_POINSIGNON.ics?version=2022.0.4.3&idICal=1C9DB9E82A019FE591DAA7372A416A6E&param=643d5b312e2e36325d2666683d3126663d31"
+
+# Connexion à la base de données SQLite
+conn = sqlite3.connect('calendriers.db')
+c = conn.cursor()
+
+# Création de la table pour stocker les iCals
+c.execute('''CREATE TABLE IF NOT EXISTS icals
+             (discord_id TEXT, ical_url TEXT)''')
+
+
+@bot.command()
+async def stocker_ical(ctx, url):
+    # Vérification de la validité de l'URL
+    regex = re.compile(
+        r'^https?://'  # http:// ou https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?|'  # domaine
+        r'localhost|'  # localhost
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # adresse IP
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    if not regex.match(url):
+        await ctx.send("URL invalide.")
+        return
+
+    # Récupération de l'ID de l'utilisateur sur Discord
+    discord_id = str(ctx.author.id)
+
+    # Ajout de l'iCal dans la base de données
+    c.execute("INSERT INTO icals VALUES (?, ?)", (discord_id, url))
+    conn.commit()
+
+    await ctx.send("iCal stocké avec succès !")
+# Commande pour récupérer l'iCal d'un utilisateur
+
+@bot.command()
+async def time(ctx):
+  date_start_str = '20220915T070000Z'
+  date_start_obj = datetime.strptime(date_start_str, '%Y%m%dT%H%M%SZ')
+
+  date_end_str = '20220915T110000Z'
+  date_end_obj = datetime.strptime(date_end_str, '%Y%m%dT%H%M%SZ')
+
+  # Définir le fuseau horaire UTC
+  timezone_utc = pytz.timezone('UTC')
+
+  # Convertir la date en heure locale
+  timezone_local = pytz.timezone('Europe/Paris') # Remplacez "Europe/Paris" par votre fuseau horaire local
+  date_start_local = timezone_utc.localize(date_start_obj).astimezone(timezone_local)
+  date_end_local = timezone_utc.localize(date_end_obj).astimezone(timezone_local)
+
+  # Afficher la date locale
+  await ctx.send(date_start_local.strftime('%Y-%m-%d %H:%M:%S'))
+  await ctx.send(date_end_local.strftime('%Y-%m-%d %H:%M:%S'))
+
+@bot.command()
+async def get_ical(ctx):
+    # Récupération de l'ID de l'utilisateur sur Discord
+    discord_id = str(ctx.author.id)
+
+    # Récupération de l'iCal dans la base de données
+    c.execute("SELECT ical_url FROM icals WHERE discord_id=?", (discord_id,))
+    row = c.fetchone()
+    if row is not None:
+        # Envoi de l'iCal à l'utilisateur
+        ical_url = row[0]
+        await ctx.send(f"Voici votre iCal: {ical_url}")
+    else:
+        await ctx.send("Aucun iCal n'a été stocké pour cet utilisateur.")
+
 
 @bot.command()
 async def bonjour(ctx):
@@ -17,13 +95,105 @@ async def bonjour(ctx):
     else:
         await ctx.send("Bonjour!")
 
+
+@bot.command()
+async def rappel(ctx, date_str, heure_str, *, message):
+    """Crée un rappel à une date et une heure spécifiques."""
+    # Convertir la date et l'heure en objets datetime
+    date_obj = datetime.strptime(date_str, '%Y-%m-%d').date()
+    heure_obj = datetime.strptime(heure_str, '%H:%M').time()
+    rappel_datetime = datetime.combine(date_obj, heure_obj)
+
+    # Calculer le temps d'attente avant le rappel
+    maintenant = datetime.now()
+    temps_attente = (rappel_datetime - maintenant).total_seconds()
+
+    # Vérifier que le temps d'attente est positif
+    if temps_attente < 0:
+        await ctx.send("La date et l'heure spécifiées sont déjà passées.")
+        return
+
+    # Attendre le temps nécessaire avant d'envoyer le rappel
+    await asyncio.sleep(temps_attente)
+
+    # Envoyer le rappel
+    await ctx.send(f"{ctx.author.mention}, voici votre rappel : {message}")
+
+
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         await ctx.send('Commande inconnue.')
     else:
         raise error
-        
+
+
+@bot.command()
+async def gpt(ctx, *, message):
+    # Envoyer la requête à l'API de ChatGPT
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=message,
+        max_tokens=50,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+
+    # Récupérer la réponse de l'API
+    reply = response.choices[0].text.strip()
+
+    # Envoyer la réponse en message privé
+    await ctx.author.send(f"Voici la réponse de ChatGPT :\n{reply}")
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+
+@bot.command()
+async def ajrd(ctx):
+  discord_id = str(ctx.author.id)
+  c.execute("SELECT ical_url FROM icals WHERE discord_id=?", (discord_id,))
+  row = c.fetchone()
+  if row is not None:
+    ical_url = row[0]
+  else:
+      await ctx.send("Aucun iCal n'a été stocké pour cet utilisateur.")
+
+  response = requests.get(ical_url)
+  today = datetime.today().date()
+  if response.status_code == 200:
+    calendar = icalendar.Calendar.from_ical(response.content)
+  for event in calendar.walk("VEVENT"):
+      if event["DTSTART"].dt.date() == today:
+        await ctx.send(event["description"])
+        break
+  else:
+      await ctx.send("Impossible de télécharger le fichier .ical")
+
+
+@bot.command()
+async def demain(ctx):
+  discord_id = str(ctx.author.id)
+  c.execute("SELECT ical_url FROM icals WHERE discord_id=?", (discord_id,))
+  row = c.fetchone()
+  if row is not None:
+    ical_url = row[0]
+  else:
+      await ctx.send("Aucun iCal n'a été stocké pour cet utilisateur.")
+  response = requests.get(ical_url)
+  tomorrow = datetime.today().date() + timedelta(days=1)
+  if response.status_code == 200:
+      calendar = icalendar.Calendar.from_ical(response.content)
+  for event in calendar.walk("VEVENT"):
+      if event["DTSTART"].dt.date() == tomorrow:
+          await ctx.send(event["description"])
+          break
+  else:
+      await ctx.send("Impossible de télécharger le fichier .ical")
+
 @bot.command()
 async def ticket(ctx):
     guild = ctx.guild
@@ -32,7 +202,8 @@ async def ticket(ctx):
         ticket_category = await guild.create_category(name='Tickets')
     overwrites = {
         guild.default_role: discord.PermissionOverwrite(read_messages=False),
-        ctx.author: discord.PermissionOverwrite(read_messages=True, send_messages=True)
+        ctx.author: discord.PermissionOverwrite(
+            read_messages=True, send_messages=True)
     }
     ticket_channel = await ticket_category.create_text_channel(name=f'ticket-{ctx.author.name}', overwrites=overwrites)
     message = await ticket_channel.send(f"{ctx.author.mention} Merci d'avoir ouvert un ticket. Veuillez choisir le rôle que vous souhaitez contacter en réagissant avec l'emoji correspondant.")
@@ -51,7 +222,8 @@ async def ticket(ctx):
             role = discord.utils.get(guild.roles, name='Pédagogie')
         elif str(reaction.emoji) == sos_emoji:
             role = discord.utils.get(guild.roles, name='SOS')
-        overwrites[role] = discord.PermissionOverwrite(read_messages=True, send_messages=True)
+        overwrites[role] = discord.PermissionOverwrite(
+            read_messages=True, send_messages=True)
         await ticket_channel.edit(overwrites=overwrites)
         await ticket_channel.send(f"Vous avez contacté {role.mention} pour vous aider. Pour fermer le ticket, cliquez sur la réaction ci-dessous.")
         await message.add_reaction('🔒')
@@ -67,4 +239,4 @@ async def ticket(ctx):
             await ticket_channel.delete()
             await ticket_channel.send(f"{ctx.author.mention} Votre ticket a été fermé.")
 
-bot.run('MTA5NTk4NzQyMDU1NTY0NDk3OA.G5tyUZ.a8IqT23mOsad3oO31BNPGdYGka41WBVesub0Qg')
+bot.run('')
